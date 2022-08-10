0ff18c7920f02d96246e1df8cb86370f5350549b
Daniel A. Wozniak <dwozniak@vmware.com>
2022-08-10 14:25:49 -0700

    Formatting and imports

--- tests/test_relok8.py
+++ tests/test_relok8.py
@@ -1,21 +1,21 @@
-
-import pytest
 from textwrap import dedent
 
-from relok8 import is_macho, is_elf, parse_otool_l, parse_readelf_d, is_in_dir
+import pytest
+
+from relok8 import is_elf, is_in_dir, is_macho, parse_otool_l, parse_readelf_d
 
 
 def test_is_macko_true(tmp_path):
-    lib_path  = tmp_path / 'test.dylib'
-    with open(lib_path, 'wb') as fp:
-        fp.write(b'\xcf\xfa\xed\xfe')
+    lib_path = tmp_path / "test.dylib"
+    with open(lib_path, "wb") as fp:
+        fp.write(b"\xcf\xfa\xed\xfe")
     assert is_macho(lib_path) == True
 
 
 def test_is_macko_false(tmp_path):
-    lib_path  = tmp_path / 'test.dylib'
-    with open(lib_path, 'wb') as fp:
-        fp.write(b'\xcf\xfa\xed\xfa')
+    lib_path = tmp_path / "test.dylib"
+    with open(lib_path, "wb") as fp:
+        fp.write(b"\xcf\xfa\xed\xfa")
     assert is_macho(lib_path) == False
 
 
@@ -25,22 +25,22 @@ def test_is_macko_not_a_file(tmp_path):
 
 
 def test_is_macko_file_does_not_exist(tmp_path):
-    lib_path = tmp_path / 'test.dylib'
+    lib_path = tmp_path / "test.dylib"
     with pytest.raises(FileNotFoundError):
         assert is_macho(lib_path) == False
 
 
 def test_is_elf_true(tmp_path):
-    lib_path  = tmp_path / 'test.so'
-    with open(lib_path, 'wb') as fp:
-        fp.write(b'\x7f\x45\x4c\x46')
+    lib_path = tmp_path / "test.so"
+    with open(lib_path, "wb") as fp:
+        fp.write(b"\x7f\x45\x4c\x46")
     assert is_elf(lib_path) == True
 
 
 def test_is_elf_false(tmp_path):
-    lib_path  = tmp_path / 'test.so'
-    with open(lib_path, 'wb') as fp:
-        fp.write(b'\xcf\xfa\xed\xfa')
+    lib_path = tmp_path / "test.so"
+    with open(lib_path, "wb") as fp:
+        fp.write(b"\xcf\xfa\xed\xfa")
     assert is_elf(lib_path) == False
 
 
@@ -50,16 +50,19 @@ def test_is_elf_not_a_file(tmp_path):
 
 
 def test_is_melffile_does_not_exist(tmp_path):
-    lib_path = tmp_path / 'test.so'
+    lib_path = tmp_path / "test.so"
     with pytest.raises(FileNotFoundError):
         assert is_elf(lib_path) == False
 
+
 def test_parse_otool_l():
-    #XXX
+    # XXX
     pass
 
+
 def test_parse_readelf_d_no_rpath():
-    section = dedent('''
+    section = dedent(
+        """
     Dynamic section at offset 0xbdd40 contains 28 entries:
       Tag        Type                         Name/Value
      0x0000000000000001 (NEEDED)             Shared library: [libz.so.1]
@@ -67,11 +70,14 @@ def test_parse_readelf_d_no_rpath():
      0x0000000000000001 (NEEDED)             Shared library: [libpng15.so.15]
      0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
      0x000000000000000e (SONAME)             Library soname: [libfreetype.so.6]
-    ''')
+    """
+    )
     assert parse_readelf_d(section) == []
 
+
 def test_parse_readelf_d_rpath():
-    section = dedent('''
+    section = dedent(
+        """
     Dynamic section at offset 0x58000 contains 27 entries:
       Tag        Type                         Name/Value
      0x000000000000000f (RPATH)              Library rpath: [$ORIGIN/../..]
@@ -79,8 +85,9 @@ def test_parse_readelf_d_rpath():
      0x0000000000000001 (NEEDED)             Shared library: [libpthread.so.0]
      0x0000000000000001 (NEEDED)             Shared library: [libc.so.6]
      0x000000000000000c (INIT)               0x51f8
-     ''')
-    assert parse_readelf_d(section) == ['$ORIGIN/../..']
+     """
+    )
+    assert parse_readelf_d(section) == ["$ORIGIN/../.."]
 
 
 def test_is_in_dir(tmp_path):
